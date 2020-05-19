59029d2a81c384da47a895bf9b461f472d007ae3
Pau Balaguer Llaquet <pbalaguer@Air-de-Pau-2.home>
2020-05-19 17:47:07 +0200

    Element names added

--- downloader/download.py
+++ downloader/download.py
@@ -10,6 +10,7 @@ websites = [BASE_URL]
 websitePattern = "http://www.visualpatterns.org/{}1-{}0.html"
 counter = 0
 solutions = {}
+types = {}
 
 for i in range(2,38, 2):
     websites.append(websitePattern.format(i, i+2));
@@ -31,17 +32,23 @@ for website in websites:
                     found = re.search('Volume in step 43 = ([0-9]+)', text).group(1)
                 else:
                     found = re.search('step 43 = ([0-9]+)', text).group(1)
+                    objType = re.search(' (.+) in step 43', text).group(1)
+                    if "step" in objType:
+                        objType = re.search(' (.+) in step 43', objType).group(1)
+                    else:
+                        objType = objType.split()[-1]
 
-                with open('../img/{}.jpg'.format(counter), 'wb') as handle:
-                    response = requests.get(image, stream=True)
-
-                    for block in response.iter_content(1024):
-                        if not block:
-                            break
-
-                        handle.write(block)
+                # with open('../img/{}.jpg'.format(counter), 'wb') as handle:
+                #     response = requests.get(image, stream=True)
+                #
+                #     for block in response.iter_content(1024):
+                #         if not block:
+                #             break
+                #
+                #         handle.write(block)
 
                 solutions[counter] = int(float(found))
+                types[counter] = objType
             except AttributeError:
                 # AAA, ZZZ not found in the original string
                 found = '' # apply your error handling
@@ -49,7 +56,11 @@ for website in websites:
             counter += 1
     print(website)
 
-json = json.dumps(solutions)
-f = open("solutions.json","w")
+# json = json.dumps(solutions)
+# f = open("solutions.json","w")
+# f.write(json)
+# f.close()
+json = json.dumps(types)
+f = open("object_types.json","w")
 f.write(json)
 f.close()
