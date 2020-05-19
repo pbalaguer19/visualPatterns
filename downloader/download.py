48f339a554281cae1231bc34635fb02aba7fef8e
Pau Balaguer Llaquet <pbalaguer@Air-de-Pau-2.home>
2020-05-19 12:28:51 +0200

    visualPatterns v1 Working

---
+++ downloader/download.py
@@ -0,0 +1,55 @@
+import requests
+import sys
+import re
+import json
+import time
+from BeautifulSoup import BeautifulSoup
+
+BASE_URL = "http://www.visualpatterns.org"
+websites = [BASE_URL]
+websitePattern = "http://www.visualpatterns.org/{}1-{}0.html"
+counter = 0
+solutions = {}
+
+for i in range(2,38, 2):
+    websites.append(websitePattern.format(i, i+2));
+
+for website in websites:
+    r = requests.get(url = website)
+    parsed_html = BeautifulSoup(r.text)
+    patterns = parsed_html.body.find('div', attrs={'class':'imageGallery'}).contents
+
+    for pattern in patterns:
+        if(str(pattern).startswith("<div")):
+            image = BASE_URL + pattern.div.div.div.div.a.get("href")
+            if "uploads" not in image:
+                continue
+            text = pattern.div.div.div.div.a.get("title")
+
+            try:
+                if "Volume" in text:
+                    found = re.search('Volume in step 43 = ([0-9]+)', text).group(1)
+                else:
+                    found = re.search('step 43 = ([0-9]+)', text).group(1)
+
+                with open('../img/{}.jpg'.format(counter), 'wb') as handle:
+                    response = requests.get(image, stream=True)
+
+                    for block in response.iter_content(1024):
+                        if not block:
+                            break
+
+                        handle.write(block)
+
+                solutions[counter] = int(float(found))
+            except AttributeError:
+                # AAA, ZZZ not found in the original string
+                found = '' # apply your error handling
+            print(counter)
+            counter += 1
+    print(website)
+
+json = json.dumps(solutions)
+f = open("solutions.json","w")
+f.write(json)
+f.close()
